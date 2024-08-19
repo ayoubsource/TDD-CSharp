56442e9dd85229605c140c5e91fc6c2e59ad32e7
Ayoub Chaabi <ayoub.chaabi01@gmail.com>
2024-08-19 15:04:53 +0200

    Refactor Encode method to use Head and EncodedDigits helper methods (#16)
    Before:
    
    	•	The Encode method handled the entire encoding process within a single function, making it harder to maintain and extend.
    	•	The method directly manipulated the input string without breaking down the logic into smaller, reusable components.
    
    After:
    
    	•	Refactored the Encode method to use two new helper methods: Head and EncodedDigits.
    	•	Head: Extracts the first letter of the word.
    	•	EncodedDigits: Encodes the remaining characters into digits, currently handling only a basic case.
    	•	This refactoring improves the readability and maintainability of the Encode method by modularizing the logic into distinct functions.
    	•	The ZeroPad method remains responsible for ensuring the output is four characters long.

--- TDD-CSharp.Sources/Soundex.cs
+++ TDD-CSharp.Sources/Soundex.cs
@@ -4,14 +4,20 @@ public class Soundex
 {
     public string Encode(string word)
     {
-        var encoded = word.Substring(0, 1);
-        if (word.Length > 1)
-        {
-            encoded += "1";
-        }
-        return ZeroPad(encoded);
+        return  ZeroPad(Head(word) + EncodedDigits(word));
     }
-    
+
+    private string EncodedDigits(string word)
+    {
+        return word.Length > 1 ? "1" : string.Empty;
+    }
+
+    private string Head(string word)
+    {
+        var encoded = word.Substring(0, 1);
+        return encoded;
+    }
+
     private string ZeroPad(string word)
     {
         var zerosNeeded = 4 - word.Length;
