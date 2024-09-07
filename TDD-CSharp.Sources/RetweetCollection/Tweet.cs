680321d42c2f0f12141d092084135a093e7b4c3e
Ayoub Chaabi <ayoub.chaabi01@gmail.com>
2024-09-07 17:59:07 +0200

    Add test to validate that a tweet user must start with ‘@’
    * Add test to validate that a tweet user must start with ‘@’ using Assert.Throws
    
    Before:
    
    	•	The Tweet class did not have a test to ensure that the user string starts with an ‘@’ symbol, leaving room for invalid usernames to be passed in without error.
    
    	•	The Tweet class did not validate that the username starts with ‘@’, allowing invalid usernames to be passed into the constructor without error.
    
    After:
    
    	•	Added validation in the Tweet constructor to ensure that the username starts with ‘@’. If the username is invalid, the constructor throws an ArgumentException.
    	•	The IsValid method checks if the username meets the required format, improving the robustness and validation of the Tweet class.
    	•	Added the RequiresUserToStartWithAtSign test to verify that an ArgumentException is thrown if the user string does not start with an ‘@’.

--- TDD-CSharp.Sources/RetweetCollection/Tweet.cs
+++ TDD-CSharp.Sources/RetweetCollection/Tweet.cs
@@ -2,5 +2,23 @@ namespace TDD_CSharp.Sources.RetweetCollection;
 
 public class Tweet
 {
-    
+    private const string NullUser = " ";
+    private readonly string _message;
+    private readonly string _user;
+
+    public Tweet(string message = "", string user = NullUser)
+    {
+        _message = message;
+        _user = user;
+
+        if (!IsValid(_user))
+        {
+            throw new ArgumentException("Invalid user: User must start with '@'.");
+        }
+    }
+
+    private bool IsValid(string user)
+    {
+        return user.StartsWith("@");
+    }
 }
