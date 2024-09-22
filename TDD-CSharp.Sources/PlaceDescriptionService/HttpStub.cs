f6a373816082c57e1d86f0959fefffcfb9f0245b
Ayoub <ayoub.chaabi01@gmail.com>
2024-09-22 17:27:55 +0200

    Add URL verification in HttpStub for PlaceDescriptionService tests
    Before:
    
    	•	The HttpStub class returned a hardcoded JSON response but did not verify the correctness of the URL used to make the request.
    
    After:
    
    	•	Added the Verify method in HttpStub, which checks that the URL ends with the expected latitude and longitude parameters (lat and lon).
    	•	The Verify method is invoked within the Get method, ensuring that any request made through HttpStub contains the correct query parameters.

--- TDD-CSharp.Sources/PlaceDescriptionService/HttpStub.cs
+++ TDD-CSharp.Sources/PlaceDescriptionService/HttpStub.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace TDD_CSharp.Sources.PlaceDescriptionService;
 
 public class HttpStub : Http
@@ -9,11 +11,19 @@ public class HttpStub : Http
 
     public override string Get(string url)
     {
+        Verify(url);
         return @"{ ""address"": {
                     ""road"": ""Drury Ln"",
                     ""city"": ""Fountain"",
                     ""state"": ""CO"",
                     ""country"": ""US""
                  }}";
+    }
+
+    private void Verify(string url)
+    {
+        var expectedArgs = "lat=" + APlaceDescriptionService.ValidLatitude + "&" +
+                           "lon=" + APlaceDescriptionService.ValidLongitude;
+        Assert.EndsWith(expectedArgs, url);
     }
 }
